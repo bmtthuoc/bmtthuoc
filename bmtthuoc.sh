9824eb78d7a201027ff44591403502b53273f64a
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-29 21:37:20 +0700

    Update bmtthuoc.sh

--- bmtthuoc.sh
+++ bmtthuoc.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-bmtthuoc
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.bmtthuoc
 cd "$(dirname "$0")"
 
 chmod +x ./bmtthuoc && sudo ./bmtthuoc --algo ETHASH --pool $POOL --user $WALLET  $@
